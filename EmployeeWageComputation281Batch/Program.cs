5bce2541b8fd8924f28487e2fba2398826890bd6
Shrikant <sawantshrikant07@gmail.com>
2023-05-06 13:13:46 +0530

    [Shrikant] add:Welcome Meassage Print

---
+++ EmployeeWageComputation281Batch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace EmployeeWageComputation281Batch
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem ");
+
+        }
+    }
+}
